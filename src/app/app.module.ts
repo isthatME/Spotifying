5b3c0573ba823bb92164237a65dc5f9b94f48386
Guilherme <guigo_gaara@hotmail.com>
2020-03-24 19:13:45 -0300

    modularizando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './fixedComponents/footer/footer.component';
+import { HeaderComponent } from './fixedComponents/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
 
 
@@ -9,6 +13,10 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
