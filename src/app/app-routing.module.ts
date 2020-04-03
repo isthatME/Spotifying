9fdc29a06eb48d118e9d5a319403f04c606b2ddc
Guilherme <guigo_gaara@hotmail.com>
2020-04-03 18:18:07 -0300

    allPlaylists done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { Playlist4Component } from './playlists/playlist4/playlist4.component';
+import { Playlist3Component } from './playlists/playlist3/playlist3.component';
+import { Playlist2Component } from './playlists/playlist2/playlist2.component';
+import { Playlist1Component } from './playlists/playlist1/playlist1.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -12,6 +16,10 @@ import { AllPlaylistsComponent } from './playlists/all-playlists/allplaylists.co
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'allPlaylists', pathMatch: 'full', component: AllPlaylistsComponent },
+  { path: 'playlist1', pathMatch: 'full', component: Playlist1Component },
+  { path: 'playlist2', pathMatch: 'full', component: Playlist2Component },
+  { path: 'playlist3', pathMatch: 'full', component: Playlist3Component },
+  { path: 'playlist4', pathMatch: 'full', component: Playlist4Component },  
   { path: 'home', pathMatch: 'full', component: HomeComponent },
   { path: 'cadastro', pathMatch: 'full', component: CadastroComponent },
   { path: 'info', pathMatch: 'full', component: InfoComponent },
