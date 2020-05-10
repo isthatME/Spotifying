6075866f7952c554088b17fc740ece81df4334ff
Guilherme <guigo_gaara@hotmail.com>
2020-05-10 19:31:37 -0300

    bug fixed

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -14,11 +14,12 @@ export class AuthGuard implements CanActivate {
     public router: Router
   ) { }
 
-  canActivate(
+  canActivate( 
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      console.log(next)
     if (this.authService.isLoggedIn != true) {
-      window.alert("Access not allowed!");
+      window.alert("Você não está logado, ou não tem uma conta válida");
       this.router.navigate(['login'])
     }
     return true;
